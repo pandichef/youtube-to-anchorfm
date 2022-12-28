58dbe8c7a821eac51ac8ebd42986121dfe543c19
pandichef <rashtyda@gmail.com>
2022-12-27 22:31:21 -0400

    chrome-extension v1

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,7 +1,29 @@
-chrome.browserAction.onClicked.addListener(function () {
-  fetch('https://youtube-to-snipd.pandibay.com?id=x7RRJhhAEfk', { method: 'GET' })
-    .then((response) => response.text())
-    .then((data) => {
-      alert(data);
-    });
+const fetchHostName = 'https://youtube-to-snipd.pandibay.com';
+
+function getCurrentTabUrl() {
+  return window.location.href;
+}
+
+chrome.browserAction.onClicked.addListener(async () => {
+  // chrome.tabs.executeScript is necessary to be able to access DOM element,
+  // in this case, the URL string of the current open tab
+  await chrome.tabs.executeScript(
+    {
+      code: '(' + getCurrentTabUrl + ')();',
+    },
+    async (results) => {
+      const currentTabUrlString = results[0];
+      let url = new URL(currentTabUrlString);
+      let topDomain = url.hostname.split('.').splice(1).join('.');
+      if (topDomain == 'youtube.com') {
+        let videoId = url.searchParams.get('v');
+        // let response = await fetch('https://jsonplaceholder.typicode.com/users', { method: 'GET' }); // For testing
+        let response = await fetch(fetchHostName + '?id=' + videoId, { method: 'GET' });
+        let responseText = await response.text();
+        alert(responseText);
+      } else {
+        alert('This extension only works with YouTube.');
+      }
+    }
+  );
 });
